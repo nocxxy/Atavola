c2f650680cdc8fe62c634f7b2e90794535cc9146
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2023-04-13 13:49:23 +0200

    Main.java: appel de la fonction getCreneau

--- App/Main.java
+++ App/Main.java
@@ -1,9 +1,14 @@
 package App;
+import Back.*;
+
+import java.sql.Statement;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Statement st = Back.connectionBase();
 		System.out.println("Hello World!");
+		Back.getCreneau(st, 1);
 	}
 
 }
