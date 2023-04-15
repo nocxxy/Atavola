a53c930ecda9c51e26c9f47cee06e86f6096df0e
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2023-04-15 17:55:03 +0200

    Main.java: test de la fonction insertCreneau

--- App/Main.java
+++ App/Main.java
@@ -8,13 +8,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Statement st = Back.connectionBase();
-		System.out.println("Hello World!");
-		Back.getCreneau(st, 1);
-		/*Date hd = new Date(2002,11,30); besoin d'aide pour le format Date sql
-		Date hf = new Date(2002,11,31);
-		Back.insertCreneau(st, hd, hf, 5);*/
-		Back.deleteCreneau(st,2);
-		Back.getAllWeeklyCreneau(st,"2023-04-21 00:00:00", "2023-04-28 00:00:00");
+
+		Back.getCreneau(st, 1); // recuperation du creneau 1
+
+		String hd = "2023-04-15 18:00:00";
+		String hf = "2023-04-15 20:00:00";
+
+		Back.insertCreneau(st, hd, hf, 1); // insertion d'un creneau avec une date de debut et date de fin
+
+		Back.deleteCreneau(st,2); // suppresion du creneau 2
+
+		Back.getAllWeeklyCreneau(st,"2023-04-21 00:00:00", "2023-04-28 00:00:00"); // recuperation de tout les creneau entre 2 dates
+
 
 	}
 
