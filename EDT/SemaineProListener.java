1d1ec5bdeca335feda7ae9811422a41c973830cb
nocxxy <evan.meziere@gmail.com>
2023-05-08 14:20:04 +0200

    enleve debug

--- EDT/SemaineProListener.java
+++ EDT/SemaineProListener.java
@@ -14,10 +14,7 @@ public class SemaineProListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("COUCOU--------------------------------------------------------------------------------------");
-        System.out.println(this.e.getDebut().toString());
         this.e.setDebut(Back.getSemaineSuivante(this.e.getDebut()));
-        System.out.println(this.e.getDebut().toString());
         this.e.setAllCreneau(Back.getAllCreneauWeek(this.e.getSt(), this.e.getDebut()));
         this.e.creeCreneauxEDT(this.e.getEmpEDT(), this.e.getEmpConn());
         this.e.creePanelBas(this.e.getEmpEDT());
